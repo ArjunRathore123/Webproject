df6d24e6bb8c7c6748d98d3aa14f2ec382b84089
ArjunRathore123 <arjun.aj8120@gmail.com>
2024-01-31 13:08:27 +0530

    requirement.txt

--- buyers/urls.py
+++ buyers/urls.py
@@ -1,11 +1,11 @@
-from .views import Home,Register,signin,signupform,handlelogin,handlelogout,account,contactus,send_email
+from .views import Home,Register,signin,signupform,handlelogin,handlelogout,contactus,send_email
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('',Home,name='home'),
     path('register/',Register,name='register'),
-    path('account/',account,name='account'),
+  
     path('login/',signin,name='login'),
     path('signup',signupform,name='signup'),
     path('login',handlelogin,name='handlelogin'),
